67b40d6e623d661c735b21e34bbe5b4276d40179
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2017-11-21 08:08:36 +0000

    [WIP, DNM] multi-worker daemon
    - [X] put multiples workers in a single binary ("-tags containerd standalone")
    - [ ] allow overriding the default worekr
    - [X] add worker selector to LLB vertex metadata
    - [ ] allow using multiples workers (requires inter-vertex cache copier, HUGE!)
    
    Implementation notes:
    - A new structure called "metaworker" holds a worker instance and its
    related stuffs such as the snapshotter
    - For containerd, we have separate workers for each of the available
    snapshotters: containerd-overlay, containerd-btrfs, ...
    - The default worker is "runc-overlay"
    
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

--- cmd/buildd/main_standalone.go
+++ cmd/buildd/main_standalone.go
@@ -1,17 +1,39 @@
-// +build standalone,!containerd
+// +build standalone
+
+// TODO(AkihiroSuda): s/standalone/runc/g
 
 package main
 
 import (
-	"github.com/moby/buildkit/control"
+	"github.com/moby/buildkit/metaworker"
+	"github.com/moby/buildkit/metaworker/runc"
 	"github.com/urfave/cli"
 )
 
-func appendFlags(f []cli.Flag) []cli.Flag {
-	return f
+func init() {
+	appFlags = append(appFlags,
+		cli.BoolFlag{
+			Name:  "disable-runc",
+			Usage: "disable runc workers",
+		})
+	metaWorkerCtors[0] = runcCtor
 }
 
-// root must be an absolute path
-func newController(c *cli.Context, root string) (*control.Controller, error) {
-	return control.NewStandalone(root)
+func runcCtor(c *cli.Context, root string) ([]*metaworker.MetaWorker, error) {
+	if c.GlobalBool("disable-runc") {
+		return nil, nil
+	}
+	opts, err := runc.NewMetaWorkerOpts(root)
+	if err != nil {
+		return nil, err
+	}
+	var mws []*metaworker.MetaWorker
+	for _, opt := range opts {
+		mw, err := metaworker.NewMetaWorker(opt)
+		if err != nil {
+			return mws, err
+		}
+		mws = append(mws, mw)
+	}
+	return mws, nil
 }
