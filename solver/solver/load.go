34f17176640310e4f06744a906ca396e2b545909
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2017-12-15 13:50:02 +0900

    revert pbload
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

--- solver/solver/load.go
+++ solver/solver/load.go
@@ -6,6 +6,7 @@ import (
 	"github.com/moby/buildkit/solver"
 	"github.com/moby/buildkit/solver/pb"
 	digest "github.com/opencontainers/go-digest"
+	"github.com/pkg/errors"
 )
 
 func newVertex(dgst digest.Digest, op *pb.Op, opMeta *pb.OpMetadata, load func(digest.Digest) (interface{}, error)) (*vertex, error) {
@@ -51,4 +52,47 @@ func llbOpName(op *pb.Op) string {
 	default:
 		return "unknown"
 	}
+}
+
+// loadLLB loads LLB.
+// fn is executed sequentially.
+func loadLLB(def *pb.Definition, fn func(digest.Digest, *pb.Op, func(digest.Digest) (interface{}, error)) (interface{}, error)) (interface{}, pb.OutputIndex, error) {
+	if len(def.Def) == 0 {
+		return nil, 0, errors.New("invalid empty definition")
+	}
+
+	allOps := make(map[digest.Digest]*pb.Op)
+
+	var dgst digest.Digest
+
+	for _, dt := range def.Def {
+		var op pb.Op
+		if err := (&op).Unmarshal(dt); err != nil {
+			return nil, 0, errors.Wrap(err, "failed to parse llb proto op")
+		}
+		dgst = digest.FromBytes(dt)
+		allOps[dgst] = &op
+	}
+
+	lastOp := allOps[dgst]
+	delete(allOps, dgst)
+	dgst = lastOp.Inputs[0].Digest
+
+	cache := make(map[digest.Digest]interface{})
+
+	var rec func(dgst digest.Digest) (interface{}, error)
+	rec = func(dgst digest.Digest) (interface{}, error) {
+		if v, ok := cache[dgst]; ok {
+			return v, nil
+		}
+		v, err := fn(dgst, allOps[dgst], rec)
+		if err != nil {
+			return nil, err
+		}
+		cache[dgst] = v
+		return v, nil
+	}
+
+	v, err := rec(dgst)
+	return v, lastOp.Inputs[0].Index, err
 }
